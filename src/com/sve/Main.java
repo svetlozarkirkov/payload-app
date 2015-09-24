ecd046ac69851a71a6b9556f856da37889513d94
Svetlozar Kirkov <svetlozark@gmail.com>
2015-09-24 18:50:22 +0300

    main reordered

--- src/com/sve/Main.java
+++ src/com/sve/Main.java
@@ -1,19 +1,29 @@
 package com.sve;
 
 import com.google.gson.Gson;
-
 import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Container testContainer = new ContainerImplementation(new BigDecimal(2), new BigDecimal(4), new BigDecimal(6));
+        Container testContainer = new ContainerImplementation(
+                new BigDecimal(2),
+                new BigDecimal(4),
+                new BigDecimal(6));
 
-        Item box =
-                new BoxImplementation("Test Item", new BigDecimal(2), new BigDecimal(4), new BigDecimal(6), new BigDecimal("158.12"));
+        Item box = new BoxImplementation(
+                "Test Item",
+                new BigDecimal(2),
+                new BigDecimal(4),
+                new BigDecimal(6),
+                new BigDecimal("158.12"));
 
-        Item cylinder = new CylinderImplementation("Cylinder", new BigDecimal(2), new BigDecimal(4), new BigDecimal("15.43"));
+        Item cylinder = new CylinderImplementation(
+                "Cylinder",
+                new BigDecimal(2),
+                new BigDecimal(4),
+                new BigDecimal("15.43"));
 
         testContainer.addItemToContainer(box);
         testContainer.addItemToContainer(cylinder);
